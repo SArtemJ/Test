aa230d7efc30ebac1abbc86bff21946b742a5be5
SArtemJ <forexample@examplesomemail321.com>
2018-02-04 19:41:51 +0300

    With all simple

---
+++ mail.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+var FromE = ""
+
+func SendEmail(to, body string) {
+	from := "test0120181@gmail.com"
+	pass := "qwertyAsdfg"
+
+	msg := "From: " + from + "\n" +
+		"To: " + to + "\n" +
+		"Subject: Bad metrics param" + "\n\n" +
+		body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		fmt.Println("error sending")
+	}
+}
